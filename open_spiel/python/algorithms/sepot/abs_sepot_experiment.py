ece197a4ac1b8c3301579c5087e753c057c0a892
David Milec <david.milec@a5labs.co>
2024-10-11 15:36:54 +0200

    Added parameters so I can run RNAD

--- open_spiel/python/algorithms/sepot/abs_sepot_experiment.py
+++ open_spiel/python/algorithms/sepot/abs_sepot_experiment.py
@@ -1,7 +1,24 @@
 import open_spiel.python.algorithms.sepot.rnad_sepot as rnad  
 import open_spiel.python.algorithms.sepot.sepot as sepot
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--batch_size", default=32, type=int, help="Batch size")
+parser.add_argument("--entropy_schedule", default=[1000, 10000], nargs="+", type=int, help="Entropy schedule")
+parser.add_argument("--entropy_schedule_repeats", default=[50, 1], nargs="+", type=int, help="Entropy schedule repeats")
+parser.add_argument("--rnad_network_layers", default=[256, 256], nargs="+", type=int, help="Network layers")
+parser.add_argument("--mvs_network_layers", default=[256, 256], nargs="+", type=int, help="Network layers")
+parser.add_argument("--transformation_network_layers", default=[256, 256], nargs="+", type=int, help="Network layers")
+parser.add_argument("--learning_rate", default=3e-4, type=float, help="Learning Rate")
+parser.add_argument("--c_vtrace", default=np.inf, type=float, help="Clipping of vtrace")
+parser.add_argument("--rho_vtrace", default=np.inf, type=float, help="Clipping of vtrace")
+parser.add_argument("--eta", default=0.2, type=float, help="Regularization term")
+parser.add_argument("--num_transformations", default=10, type=int, help="Transformations of both players")
 
 def main():
+  args = parser.parse_args([] if "__file__" not in globals() else None)
+
   game_name = "goofspiel"
   game_params = (
         ("num_cards", 3),
@@ -12,23 +29,23 @@ def main():
   max_trajectory = 4 
   rnad_config = rnad.RNaDConfig(
       game_name = game_name, 
-      game_params = game_params,
       trajectory_max =  max_trajectory,
-      policy_network_layers = 0,
-      mvs_network_layers = 0,
-      transformation_network_layers = 0,
+      policy_network_layers = args.rnad_network_layers,
+      mvs_network_layers = args.mvs_network_layers,
+      transformation_network_layers = args.transformation_network_layers,
       
-      batch_size = 0,
-      learning_rate = 0,
-      entropy_schedule_repeats = 0,
-      entropy_schedule_size = 0,
-      c_vtrace = 0,
-      rho_vtrace = 0,
-      eta_reward_transform = 0,
+      batch_size = args.batch_size,
+      learning_rate = args.learning_rate,
+      entropy_schedule_repeats = args.entropy_schedule_repeats,
+      entropy_schedule_size = args.entropy_schedule,
+      c_vtrace = args.c_vtrace,
+      rho_vtrace = args.rho_vtrace,
+      eta_reward_transform = args.eta,
 
-      num_transformations = 0,
+      num_transformations = args.num_transformations,
       matrix_valued_states = True,
-      seed = 0
+      seed=  args.seed,
+      state_representation = rnad.StateRepresentation.INFO_SET
   )
   
   sepot_config = sepot.SePoTConfig(
